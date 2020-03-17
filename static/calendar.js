b4a8c1849cf4ac50e942e25c9b455862469913b3
Shimolee Jhaveri <shimoleejhaveri@gmail.com>
2020-03-17 04:46:50 +0000

    CSS and JavaScript files

--- static/calendar.js
+++ static/calendar.js
@@ -26,9 +26,9 @@ function initClient() {
         gapi.auth2.getAuthInstance().signOut();
       });
     },
-    (err) => {
-      alert(`We couldn't log you in for some reason.\n${JSON.stringify(error, null, 2)}`);
-    }
+    // (err) => {
+    //   alert(`We couldn't log you in for some reason.\n${JSON.stringify(error, null, 2)}`);
+    // }
   );
 }
 
